5265c8e426e60d15959e9f1d00db1130a0889cd6
Gerardo I Contreras <gic@MacBook-Pro.lan>
2022-04-16 00:43:15 -0500

    added and changed files

---
+++ easy.js
@@ -0,0 +1,7 @@
+var name1 = "Thomas";
+var name1 = "John";
+let length1 = name1.length;
+let length2 = name2.length;
+var diff = length1 - length2;
+
+console.log("The name Thomas is longer than John by " + diff + " characters.");
---
+++ veryEasy.js
@@ -0,0 +1,4 @@
+var num1 = 6;
+var num2 = 2;
+var diff = num1 - num2;
+console.log("The difference between " + num1 + " and " + num2 + " is " + diff);
