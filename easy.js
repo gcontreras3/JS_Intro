4d7643990e3a8f3e271239f9935ced3dddf10d7f
Gerardo I Contreras <gic@MacBook-Pro.lan>
2022-04-18 19:16:17 -0500

    made final changes to files

--- easy.js
+++ easy.js
@@ -1,5 +1,5 @@
 var name1 = "Thomas";
-var name1 = "John";
+var name2 = "John";
 let length1 = name1.length;
 let length2 = name2.length;
 var diff = length1 - length2;
--- medium.js
+++ medium.js
@@ -1,9 +1,8 @@
 var input = prompt('Type a phrase');
-console.log("hello")
 
-if (input == input.toUpperCase) {
+if (input == input.toUpperCase()) {
     console.log("You are shouting");
-} else if (input == input.toLowerCase) {
+} else if (input == input.toLowerCase()) {
     console.log("You are whispering");
 } else {
     console.log('No input');
